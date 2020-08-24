f45c4cd98e25d93e78a9a8f047d9339990483ccf
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-08-23 21:24:52 -0400

    made endpoints in carsRouter

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -3,4 +3,38 @@ const db = require("../data/config");
 
 const router = express.Router();
 
+// endpoints
+
+// Get Cars
+router.get("/", async (req, res, next) => {
+    try {
+        res.json(await db("cars"))
+    } catch (err) {
+        next(err)
+    }
+});
+
+// Get Car by ID
+router.get("/:id", async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const car = await db("cars").where("id", id).first();
+        res.json(car);
+    } catch (err) {
+        next(err)
+    }
+});
+
+// Create Car
+router.post("/", async (req, res, next) => {
+    try {
+        const [id] = await db("cars").insert(req.body);
+        const newCar = await db("cars").where("id", id).first();
+
+        res.status(201).json(newCar);
+    } catch (err) {
+        next(err)
+    }
+});
+
 module.exports = router;
