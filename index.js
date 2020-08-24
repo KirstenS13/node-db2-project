a1521acaee59b7f3347bacca42a538a7cd12797d
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-08-23 21:09:19 -0400

    made welcome router and cars router, added both to index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const carsRouter = require("./cars/carsRouter");
+const welcomeRouter = require("./welcome/welcomeRouter");
 
 const server = express();
 const port = process.env.PORT || 5000;
 
 server.use(express.json());
+
+server.use(welcomeRouter);
+server.use("/cars", carsRouter);
+
 server.use((err, req, res, next) => {
     console.log(err);
     res.status(500).json({
